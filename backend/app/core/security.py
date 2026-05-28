b2f1f7caa574fd07eff6426990d1ac704dd450bd
Vivek Sharma <227633719+vivekcyr25@users.noreply.github.com>
2026-05-29 01:51:21 +0530

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -11,7 +11,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password[:72])
